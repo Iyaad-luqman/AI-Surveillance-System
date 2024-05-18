12eea602d480665ffcfbc3e1e59e05ed7e0605b4
init_6 <iyaadluq@gmail.com>
2024-05-18 21:57:25 +0530

    Finalising html code

--- main.py
+++ main.py
@@ -1,35 +1,23 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from query_process import process_prompt
 app = Flask(__name__)
 from classify_video import classify_videos
 import json
 
-# @app.route("/api/nlp/process")
-# def nlp_process():
-#     data = request.args.get('data')
-#     json_schema = '''{
-#   "type": "object",
-#   "properties": {
-#     "categories": {
-#       "type": "array",
-#       "items": {
-#         "type": "string"
-#       }
-#     },
-#     "true_case": {
-#       "type": "array",
-#       "items": {
-#         "type": "string"
-#       }
-#     }
-#   },
-#   "required": ["categories", "true_case"]
-# }'''
-#     return process_prompt(data, model_name = "gemini", json_schema = json_schema)
+
+@app.route("/")
+def index():
+  return render_template('index.html')
 
 
+
+
+
+
+@app.route("/api/nlp/process")
 def nlp_process():
-    data = "Is there any car crash in the video ?"
+    data = request.args.get('data')
+    test_name = request.args.get('test_name')
     json_schema = '''{
   "type": "object",
   "properties": {
@@ -39,21 +27,20 @@ def nlp_process():
         "type": "string"
       }
     },
-    "type": {
+    "true_case": {
       "type": "array",
       "items": {
         "type": "string"
       }
     }
   },
-  "required": ["categories", "type"]
+  "required": ["categories", "true_case"]
 }'''
+    json_response = nlp_process()
+    json_data = json.loads(json_response)
+    print(json_response)
+    categories_list = json_data.get('categories', [])
+    type_list = json_data.get('type', [])
+    classify_videos("testing-data/accident.mp4", categories_list, type_list)
     return process_prompt(data, model_name = "gemini", json_schema = json_schema)
 
-
-json_response = nlp_process()
-json_data = json.loads(json_response)
-print(json_response)
-categories_list = json_data.get('categories', [])
-type_list = json_data.get('type', [])
-classify_videos("testing-data/accident.mp4", categories_list, type_list)
